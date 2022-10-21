ac596ac5981e4c3df966b7d3090ae3f903152eee
Ramiro Reche <reche.ramiro98@gmail.com>
2022-10-21 20:32:01 -0300

    First commit. Initial rules, notes, and instructions were added

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Esta aplicación fue realizada en mis comienzos con python. La idea es practicar el lenguaje, la lógica, e iniciar mi primer proyecto con python.
+
+- Nombre del juego: Piedra, papel, o tijera
+- Cantidad jugadores: 2
+- Cantidad elementos: 3
+- Elementos:
+    - piedra
+    - papel
+    - tijera
+- Reglas:
+    - piedra le gana a tijera, pero pierde con papel
+    - tijera le gana a papel, pero pierde con piedra
+    - papel le gana a piedra, pero pierde con tijera
+    - las tiradas pueden ser infinitas hasta que uno de los 2 jugadores abandone la partida
+
+
+
+NOTAS: 
+- puedo empezar con un diccionario, y en cada key que haya un array, donde el índice 0 siempre sea la respuesta incorrecta.
+"""
+
